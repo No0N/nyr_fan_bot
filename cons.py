a98fe4d1be421031eb868de423a7bd5c6c5acf14
No0N <noon@live.ru>
2024-01-24 19:08:32 +0300

    Update cons.py

--- cons.py
+++ cons.py
@@ -1,3 +1,3 @@
-token_tg = '5186638664:AAH8ZlHOpviSXpKoT-rxV-rHgRJxWW6xRgk'
-chat_id = '-1001574525289'
+token_tg = ''
+chat_id = ''
 xml_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCVhibwHk4WKw4leUt6JfRLg'
