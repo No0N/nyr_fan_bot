da455d54deeebcf13e1d5bc97c70397c13327837
Danilov Yura <noon@live.ru>
2024-02-11 09:05:42 +0300

    main

--- tg_nyr.py
+++ tg_nyr.py
@@ -3,10 +3,9 @@ import urllib.request
 import xml.etree.ElementTree as ET
 import time
 import sqlite3
+from datetime import datetime
 import pytz
-from datetime import datetime, timedelta
 from cons import xml_url, token_tg, chat_id
-
 
 bot = telebot.TeleBot(token_tg)
 
@@ -44,56 +43,44 @@ def check_if_record_exists(conn, title, pub_date, video_url, post_content):
     ''', (video_url,))
     return cursor.fetchone() is not None
 
-def send_message_to_channel(schedule_time):
+def send_message_to_channel():
     try:
-        conn = sqlite3.connect(database_file)
-        cursor = conn.cursor()
+        moscow_timezone = pytz.timezone('Europe/Moscow')
+        current_time = datetime.now(moscow_timezone)
+        current_hour = current_time.hour
+        current_minute = current_time.minute
 
-        # Выбираем первую запись с пустым полем post
-        cursor.execute('SELECT id, title, url FROM videos WHERE post IS NULL OR post = "" LIMIT 1')
-        row = cursor.fetchone()
+        if 9 <= current_hour < 10 and current_minute <= 10:
+            conn = sqlite3.connect(database_file)
+            cursor = conn.cursor()
 
-        print(f"Row from database: {row}")
+            cursor.execute('SELECT id, title, url FROM videos WHERE post IS NULL OR post = "" LIMIT 1')
+            row = cursor.fetchone()
 
-        # Получаем текущее время в Московском времени
-        current_time = datetime.now(pytz.timezone('Europe/Moscow'))
-        
-        print(f"CT={current_time}, ST={schedule_time}") 
-        
-        if row is not None:
-            video_id, video_title, video_url = row  # Поля с ID, названием и URL видео
+            print(f"Row from database: {row}")
 
-            # Планируем отправку сообщения в заданное время
-            if current_time >= schedule_time:
-                # Убираем дату из названия
+            if row is not None:
+                video_id, video_title, video_url = row
+
                 video_title_cleaned = video_title.split(' - ')[0]
-                
-                # Отправляем сообщение с названием и ссылкой в основной чат
+
                 message_text_main_chat = f"{video_title_cleaned}\n\n{video_url}"
                 bot.send_message(chat_id=chat_id, text=message_text_main_chat)
                 print("Сообщение успешно отправлено в основной чат.")
 
-                # Проставляем в поле post значение 'X' для выбранной строки
                 update_post_status(conn, video_id)
             else:
-                print("Время для отправки еще не наступило.")
-                jojotime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                print(jojotime)
-                print(f"CT={current_time}, ST={schedule_time}") 
+                print("Нет записей с пустым полем post.")
 
+            conn.close()
         else:
-            print("Нет записей с пустым полем post.")
-
-        conn.close()
+            print("Время не в заданном диапазоне для отправки сообщения.")
+            print(f"CT{current_time}")
+            print(f"CTH{current_hour}")
+            print(f"CTM{current_minute}")
 
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
-
-# Функция для конвертации scheduled_datetime во время сервера
-def convert_to_server_time(scheduled_datetime):
-    server_timezone = pytz.timezone('Europe/Moscow')  # Ваш часовой пояс сервера
-    server_time = datetime.now(server_timezone)
-    return scheduled_datetime.astimezone(server_timezone)
 
 def parse_xml_and_save_to_db(url, db_file):
     conn = sqlite3.connect(db_file)
@@ -101,50 +88,36 @@ def parse_xml_and_save_to_db(url, db_file):
 
     while True:
         try:
-            # Получаем данные XML по ссылке
             response = urllib.request.urlopen(url)
             xml_data = response.read()
 
-            # Парсим XML
             root = ET.fromstring(xml_data)
 
-            # Счетчик добавленных записей
             added_rows_count = 0
 
-            # Проходим по элементам и записываем данные в базу данных
             for entry in root.findall('.//{http://www.w3.org/2005/Atom}entry'):
                 title = entry.find('{http://www.w3.org/2005/Atom}title').text
                 pub_date = entry.find('{http://www.w3.org/2005/Atom}published').text
                 video_url = entry.find('{http://www.w3.org/2005/Atom}link').attrib['href']
 
-                # Проверяем наличие элемента <content>
                 content_element = entry.find('{http://www.w3.org/2005/Atom}content')
                 post_content = content_element.text if content_element is not None else ""
 
-                # Проверяем, существует ли запись с такими же данными в базе данных, и проверяем, содержатся ли ключевые слова в заголовке
                 if not check_if_record_exists(conn, title, pub_date, video_url, post_content) and \
                     ("NHL Highlights" in title) and ("Rangers" in title):
-                    # Выводим информацию в формате ключ:значение
+
                     print(f"Title: {title}")
                     print(f"pubDate: {pub_date}")
                     print(f"url: {video_url}")
                     print(f"post: {post_content}")
                     print()
 
-                    # Записываем данные в базу данных
                     insert_data(conn, title, pub_date, video_url, post_content)
                     added_rows_count += 1
 
-            # Конвертируем время сервера
-            server_time = convert_to_server_time(scheduled_datetime)
+            send_message_to_channel()
 
-            # Проверяем, больше ли текущее время сервера или равно scheduled_datetime
-            if server_time >= scheduled_datetime:
-                # Отправляем сообщение
-                send_message_to_channel(server_time)
-
-            # Ждем 5 минут перед следующим опросом
-            time.sleep(300)  # 300 секунд = 5 минут
+            time.sleep(300)
 
         except Exception as e:
             print(f"Ошибка при обработке XML данных: {e}")
@@ -153,11 +126,4 @@ def parse_xml_and_save_to_db(url, db_file):
 
 if __name__ == "__main__":
     database_file = "videos_database.db"
-    
-    # Указываем время в MSK для отправки сообщения (в формате HH:MM)
-    scheduled_time_utc = datetime.strptime("01:00", "%H:%M").time()
-    
-    # Конвертируем в московское время
-    scheduled_datetime = datetime.combine(datetime.today(), scheduled_time_utc).astimezone(pytz.timezone('Europe/Moscow'))
-
     parse_xml_and_save_to_db(xml_url, database_file)
