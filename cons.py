ed40f04d4a27e5bf2423bc5809eee9fb68f1cc21
Danilov Yura <noon@live.ru>
2024-02-11 19:33:44 +0300

    main

--- cons.py
+++
@@ -1,5 +0,0 @@
-token_tg = '5186638664:AAH8ZlHOpviSXpKoT-rxV-rHgRJxWW6xRgk'
-chat_id = '-1001255191809'
-chat_id_tmp = '-1001574525289'
-xml_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCVhibwHk4WKw4leUt6JfRLg'
-tt_test = 1
